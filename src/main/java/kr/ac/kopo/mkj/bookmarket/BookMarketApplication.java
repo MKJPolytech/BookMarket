0e0216809961601dd886c02d68e03eae5a45a002
MKJPolytech <mkjpolytech@daum.net>
2025-03-12 11:27:50 +0900

    Initial commit

---
+++ src/main/java/kr/ac/kopo/mkj/bookmarket/BookMarketApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.mkj.bookmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMarketApplication.class, args);
+    }
+
+}
